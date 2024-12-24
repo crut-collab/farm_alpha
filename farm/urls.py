1c2d1ee533af37f900eb0dd4bebc2fb3ecd29fa5
crut-collab <k.statuev@refor.by>
2024-12-24 16:36:59 +0300

    Delete farm directory

--- farm/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
-
-urlpatterns = [
-	path('', include('web.urls')),
-	path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
